3d1a408f65d312faee5010080ae5f8d0508da650
Coffeboi <cscoffe1997@gmail.com>
2022-10-25 23:30:29 +0200

    big changes

--- index.js
+++ index.js
@@ -3,69 +3,114 @@
 import colors from 'colors';
 import fetch from 'node-fetch';
 import promptSync from 'prompt-sync';
-colors.enable();
 const prompt = promptSync(); ({ sigint: true });
+colors.enable();
 
 //Clears the console
 function clearConsoleAndScrollbackBuffer() {
     process.stdout.write("\u001b[3J\u001b[2J\u001b[1J"); console.clear();
 }
 
-//Prompts user about address.
-async function addressPrompt() {
-    console.log('Enter XKR address: ')
-    let address = prompt();
-    clearConsoleAndScrollbackBuffer()
-    return address;
-}
-
 //Prompts user about pool.
 async function getPool() {
-    console.log('Which pool are you mining on?')
+    clearConsoleAndScrollbackBuffer()
+    console.log('Which pool are you mining on?'.green)
     console.log('1. NorPool')
     console.log('2. SwePool')
     console.log('3. Gamersnest')
     console.log('4. Göta Pool')
     let pool = prompt();
-    clearConsoleAndScrollbackBuffer()
     return pool;
+}
 
+//Prompts user about address.
+async function addressPrompt() {
+    clearConsoleAndScrollbackBuffer()
+    console.log('Enter XKR address: ')
+    let address = prompt();
+    return address;
 }
 
 //Handles pool prompt.
 async function poolPicker(pool) {
     if (pool = 1) {
-        norpoolStats();
+        let address = await addressPrompt();
+
+        async function norpoolData() {
+            clearConsoleAndScrollbackBuffer()
+            const response = await fetch('https://norpool.org/api/stats_address?address=' + address);
+            const data = await response.json()
+            clearConsoleAndScrollbackBuffer()
+            console.log('Worker stats for ' + address);
+            console.log('--------------------'.red);
+            for (const worker in data.workers) {
+                console.log('name: '.green + (data.workers[worker]["name"]));
+                console.log('hashrate: '.yellow + ((data.workers[worker]["hashrate"] / 1000) + 'KH/s'));
+                console.log('--------------------'.red);
+            }
+        }
+        setInterval(norpoolData, 5000);
     }
+
     else if (pool = 2) {
-        swepoolStats();
+        let address = await addressPrompt();
+
+        async function swepoolData() {
+            clearConsoleAndScrollbackBuffer()
+            const response = await fetch('https://swepool.org/api/stats_address?address=' + address);
+            const data = await response.json();
+            console.log('Worker stats for ' + address);
+            console.log('--------------------'.red);
+            for (const worker in data.workers) {
+                console.log('name: '.green + (data.workers[worker]["name"]));
+                console.log('hashrate: '.yellow + ((data.workers[worker]["hashrate"] / 1000) + 'KH/s'));
+                console.log('--------------------'.red);
+            }
+        }
+        setInterval(swepoolData, 5000);
     }
+
     else if (pool = 3) {
-        gamersnestStats();
+        let address = await addressPrompt();
+
+        async function gnData() {
+            clearConsoleAndScrollbackBuffer()
+            const response = await fetch('https://pool.gamersnest.org/api/stats_address?address=' + address);
+            const data = await response.json();
+            console.log('Worker stats for ' + address);
+            console.log('--------------------'.red);
+            for (const worker in data.workers) {
+                console.log('name: '.green + (data.workers[worker]["name"]));
+                console.log('hashrate: '.yellow + ((data.workers[worker]["hashrate"] / 1000) + 'KH/s'));
+                console.log('--------------------'.red);
+            }
+        }
+        setInterval(gnData, 5000);
     }
+
     else if (pool = 4) {
-        götaStats();
+        let address = await addressPrompt();
+
+        async function götaData() {
+            clearConsoleAndScrollbackBuffer()
+            const response = await fetch('https://pool.kryptokrona.se/api/stats_address?address=' + address);
+            const data = await response.json();
+            console.log('Worker stats for ' + address);
+            console.log('--------------------'.red);
+            for (const worker in data.workers) {
+                console.log('name: '.green + (data.workers[worker]["name"]));
+                console.log('hashrate: '.yellow + ((data.workers[worker]["hashrate"] / 1000) + 'KH/s'));
+                console.log('--------------------'.red);
+            }
+        }
+        setInterval(götaData, 5000);
     }
+
     else {
         console.log('Whoops! Please enter a number from 1 to 4.');
-        return pool;
-    };
-}
-
-//Function for fetching pool url api.
-async function norpoolStats() {
-    let address = await addressPrompt();
-    const response = await fetch('https://norpool.org/api/stats_address?address=' + address);
-    const data = await response.json();
-    let workers = data.workers;
-
-    console.log('NorPool worker stats for ' + address);
-    for (const worker in data.workers) {
-        console.log('name: '.green + (data.workers[worker]["name"]));
-        console.log('hashrate: '.yellow + ((data.workers[worker]["hashrate"] / 1000) + 'KH/s'));
-        console.log('--------------------'.red);
+        return;
     }
-}
+};
 
 async function main() {
     getPool();
